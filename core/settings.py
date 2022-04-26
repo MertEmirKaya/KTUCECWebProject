7f26c721cba3df0725e09088490a213324caefd7
Mert Kaya <mertemir_54@outlook.com>
2022-04-26 17:04:02 +0300

    added cors settings

--- core/settings.py
+++ core/settings.py
@@ -135,3 +135,5 @@ MEDIA_URL='/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
