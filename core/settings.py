55002335fb1bf7b84d5088d35fd912fce06e93c1
Mert Kaya <mertemir_54@outlook.com>
2022-04-26 16:58:35 +0300

    added cors settings

--- core/settings.py
+++ core/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    
 
 ]
 
