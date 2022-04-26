014d24de772700da09988a31543e28c3a4e939c3
Mert Kaya <mertemir_54@outlook.com>
2022-04-26 16:16:27 +0300

    prepared for heroku

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'core.urls'
