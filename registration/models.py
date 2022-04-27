45ce6e46f8fa4fbf3eeb6ce46bf0e2faa327e314
Mert Kaya <mertemir_54@outlook.com>
2022-04-27 13:23:17 +0300

    added role field to ProfileModel

--- registration/models.py
+++ registration/models.py
@@ -12,15 +12,15 @@ class ProfileModel(AbstractUser):
     id = models.UUIDField(primary_key = True,default = uuid.uuid4,editable = False)
     bio=models.TextField(null=True,blank=True)
     school_number=models.CharField(max_length=20,unique=True,null=True,blank=True)
-    phone=models.CharField(max_length=20,unique=True)
+    phone=models.CharField(max_length=20,unique=True,null=True,blank=True)
     departmand=models.CharField(max_length=50,null=True,blank=True)
-    classes=[('Hazırlık Sınıfı','Hazırlık Sınıfı'),('Birinci Sınıf','Birinci Sınıf'),('İkinci Sınıf','İkinci Sınıf'),('Üçüncü Sınıf','Üçüncü Sınıf'),('Dördüncü Sınıf','Dördüncü Sınıf'),('4+','4+')]
+    classes=[('0','Hazırlık Sınıfı'),('1','Birinci Sınıf'),('2','İkinci Sınıf'),('3','Üçüncü Sınıf'),('4','Dördüncü Sınıf'),('5','4+')]
     grade=models.CharField(max_length=20,choices=classes,null=True,blank=True)
     fee=models.BooleanField(default=False,null=True,blank=True)
     created_time=models.DateTimeField(auto_now_add=True)
     register_date=models.DateField(null=True,blank=True)
     image=models.ImageField(null=True,blank=True,upload_to=upload_to)
-
+    role=models.CharField(max_length=90,default='member')
 
     def __str__(self) -> str:
         return self.username   
