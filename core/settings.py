14e2ab21eb5e5e4e583e375c8ba598dd90402cb4
Mert Kaya <mertemir_54@outlook.com>
2022-06-15 11:16:12 +0300

    trying to fixed bugs

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,6 +145,7 @@ STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
+django_heroku.settings(locals())
 
 
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
