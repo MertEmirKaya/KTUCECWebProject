78563b42a55eabde0b9c8e0b2664fea255d2d4e6
Mert Kaya <mertemir_54@outlook.com>
2022-06-15 14:21:37 +0300

    gonna us sqlite3

--- core/settings.py
+++ core/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-     'default': {
-         'ENGINE':'django.db.backends.postgresql',
-         'NAME':'d2a4p4vp6sh7kq',
-         'USER':'lnguzzcdpkuctg',
-         'PASSWORD':'cfb17c0afd906ffe7d40a993f08886939c277b0221b095b4fcfcfc010d08c56e',
-         'HOST':'ec2-3-248-121-12.eu-west-1.compute.amazonaws.com',
-         'PORT':'5432',
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# DATABASES = {
+#      'default': {
+#          'ENGINE':'django.db.backends.postgresql',
+#          'NAME':'d2a4p4vp6sh7kq',
+#          'USER':'lnguzzcdpkuctg',
+#          'PASSWORD':'cfb17c0afd906ffe7d40a993f08886939c277b0221b095b4fcfcfc010d08c56e',
+#          'HOST':'ec2-3-248-121-12.eu-west-1.compute.amazonaws.com',
+#          'PORT':'5432',
+#      }
+#  }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
