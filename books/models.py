49997b21510f52a8916e5282171e0e7b6fc8caf0
Mert Kaya <mertemir_54@outlook.com>
2022-06-15 12:07:57 +0300

    changed books

--- books/models.py
+++ books/models.py
@@ -1,6 +1,4 @@
-from django.db import models
 
-# Create your models here.
 from django.db import models
 from django.conf import settings
 # Create your models here.
--- core/wsgi.py
+++ core/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
 
 
