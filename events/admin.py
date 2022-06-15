20cda6a2a07d6c9841a53070d297c54f1479c916
Mert Kaya <mertemir_54@outlook.com>
2022-06-15 14:23:52 +0300

    gonna us sqlite3

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from.models import EventModel, ImageModel,EventAlbumModel,VideoModel
 
 # Register your models here.
+
+admin.site.register(EventModel)
+
+admin.site.register(ImageModel)
+
+admin.site.register(EventAlbumModel)
+
+admin.site.register(VideoModel)
